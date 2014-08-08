a4ad3b78ed758b4ee81771155900dd2fb33a8aa7
shehryar <ali.shehryar@live.com>
2014-08-08 16:01:18 +0500

    polyline added

--- app/assets/javascripts/map_LatLng.js
+++ app/assets/javascripts/map_LatLng.js
@@ -1,4 +1,3 @@
-
 var marker;
 var mapProp;
 var map;
@@ -11,6 +10,8 @@ var lat2;
 var lng2;
 var marker2;
 var loc2 = new google.maps.LatLng(0,0.5);
+var path;
+var path_line;
 
 function initialize() {
   mapProp = {
@@ -35,7 +36,14 @@ marker2 = new google.maps.Marker({
     
 });
 
-marker2.setMap(map);    
+marker2.setMap(map);   
+    
+//plot the polyline path between the two markers
+    init_path();
+//set the path_line
+    set_path();
+    
+    
 
 }
 
@@ -48,14 +56,16 @@ function pan() {
   lat2 = document.getElementById("latitude2").value;
   lng2 = document.getElementById("longitude2").value;    
   //Condition added to account for -90<lat<90 and -180<lng<180.    
-  if (((lat||lat2) > 90 || (lat||lat2) < -90) || ((lng||lng2) <-180 || (lng||lng2) > 180)) {
+  if (((lat||lat2)>90||(lat||lat2)<-90)||((lng||lng2)<-180||(lng||lng2)>180)) {
     alert("The Limits for either Latitude or Longitude are out of bound.Please Try Again!");
       clrTxt();
   } 
   else {
 //before changing map, markers set to null to erase previous marker instance.
     marker.setMap(null);
-    marker2.setMap(null);  
+    marker2.setMap(null); 
+//Erase previous path line
+    path_line.setMap(null);
 //before changing map, previous coordinates clear
     clrTxt();
 //new center defined according to the coordinated input for LatLng & loc2 is the location of 2nd pair of coordinates
@@ -84,14 +94,17 @@ function pan() {
       }
 
       map.fitBounds(new_boundary);
-  }
-    
 //map panned to new coordinates.
-        map.panTo(new_boundary);    
+      map.panTo(new_boundary);    
+//plot the polyline path between the two markers
+init_path();
+set_path();
+  }
 
 }
 
-//function created to clear text boxes
+
+//function created to clear text boxes//////////////////////////////////////////////////////////////////////////////////////////////
 function clrTxt() {
 
   document.getElementById("latitude").value = "";
@@ -99,4 +112,19 @@ function clrTxt() {
   document.getElementById("latitude2").value = "";
   document.getElementById("longitude2").value = "";    
 
+}
+//function to initialize polyline path///////////////////////////////////////////////////////////////////////////////////////////////
+function init_path(){
+    path = [myCenter,loc2];
+    path_line = new google.maps.Polyline({
+        path: path,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.6,
+        strokeWeight: 4
+        });
+}
+
+//function to add path////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function set_path(){
+    path_line.setMap(map);
 }
